41561155a2e85339b1ff9e0961cceb9ac74f3e74
mobin <fermow.fermow85@gmail.com>
2025-04-17 00:20:15 +0330

    Update Main.java

--- src/main/java/org/AP/Main.java
+++ src/main/java/org/AP/Main.java
@@ -4,16 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // بارگذاری داده‌ها از فایل
-        Scanner scanner = new Scanner(System.in);  // ایجاد شیء Scanner در اینجا
+       
+        Scanner scanner = new Scanner(System.in);  
 
         Database.loadAccounts();
         Database.loadSongs();
         Database.loadAlbums();
         Database.migrateOldComments();
-
-
-        // شروع برنامه
         System.out.println("Welcome to Genius!");
         MainMenu.show(scanner);
     }
